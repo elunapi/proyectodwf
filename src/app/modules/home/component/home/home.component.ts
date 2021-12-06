5edd31e4f5c2854a9a6de01d898533d9c3d8f8f8
System Administrator <root@MacBook-Pro-de-Enrique.local>
2021-12-05 22:18:56 -0600

    Implementacion de productos aleatorios en home

--- src/app/modules/home/component/home/home.component.ts
+++ src/app/modules/home/component/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/modules/product/_model/product';
+import { ProductImage } from 'src/app/modules/product/_model/productImage';
+import { ProductRandom } from '../../_model/ProductRandom';
+import { RandomProductsService } from '../../_service/random-products.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // Datos del producto
+  products: ProductRandom[] = [];
+
+  constructor(
+    private product_service: RandomProductsService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.product_service.getProducts().subscribe(
+      res => {
+        this.products = res;
+        console.log(res)
+      },
+      err => console.log(err)
+    )
   }
 
 }
