110892a8b0d89f26553aa8d143c040320f74cecf
System Administrator <root@MacBook-Pro-de-Enrique.local>
2021-12-06 23:12:53 -0600

    implementacion de vista de productos por categoria

--- src/app/modules/home/component/home/home.component.ts
+++ src/app/modules/home/component/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Category } from 'src/app/modules/product/_model/category';
 import { Product } from 'src/app/modules/product/_model/product';
 import { ProductImage } from 'src/app/modules/product/_model/productImage';
+import { CategoryService } from 'src/app/modules/product/_service/category.service';
 import { ProductRandom } from '../../_model/ProductRandom';
+import { CategoryProductsService } from '../../_service/category-products.service';
 import { RandomProductsService } from '../../_service/random-products.service';
 
 @Component({
@@ -13,25 +16,57 @@ import { RandomProductsService } from '../../_service/random-products.service';
 export class HomeComponent implements OnInit {
 
   // Datos del producto
-  products: ProductRandom[] = [];
+  rproducts: ProductRandom[] = [];
+  //products: Product[] = [];
+  categories: Category[] = [];
 
   constructor(
     private product_service: RandomProductsService,
+    private category_service: CategoryService,
+    private category_product_service: CategoryProductsService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
     this.getProducts();
+    this.getCategories();
   }
 
   getProducts(){
     this.product_service.getProducts().subscribe(
       res => {
-        this.products = res;
+        this.rproducts = res;
+        //console.log(res)
+      },
+      err => console.log(err)
+    )
+  }
+
+  getCategories(){
+    this.category_service.getCategories().subscribe(
+      res => {
+        this.categories = res;
         console.log(res)
       },
       err => console.log(err)
     )
+  }
+
+  muestraCategoria(id: number){
+    if(id==0){
+      this.getProducts();
+    }
+    this.category_product_service.getProducts(id).subscribe(
+      res => {
+        this.rproducts = res;
+        console.log(res)
+      },
+      err => console.log(err)
+    )
+  }
+
+  muestraCategorias(){
+    console.log("hola");
   }
 
 }
